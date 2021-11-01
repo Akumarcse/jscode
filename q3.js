c19df7cd8ae5dde605621b03dad5b3a5bce2d706
Akumarcse <90023025+Akumarcse@users.noreply.github.com>
2021-11-01 20:34:50 +0530

    Add files via upload

---
+++ q3.js
@@ -0,0 +1,10 @@
+var a;
+console.log(a);
+//console.log(b); --> here b is undefined variable 
+
+a= 57;
+if(a==undefined){
+    console.log("a is undefined ");
+}else{
+    console.log("a is defined and its value is "+a);
+}
