782646662c38b0d184a9f9cf5712d2ac301af726
Akumarcse <90023025+Akumarcse@users.noreply.github.com>
2021-11-27 18:07:17 +0530

    Add files via upload

---
+++ asst.js
@@ -0,0 +1,29 @@
+var Person1 = new Object()
+Person1.FirstName = "Amitesh",
+    Person1.LastName = "Kumar",
+    Person1.OfficeAddress = "Patna,Bihar"
+
+var Person2 = new Object()
+Person2.FirstName = "Ankit",
+    Person2.LastName = "Kumar",
+    Person2.OfficeAddress = "Patna,Bihar"
+
+var Person3 = new Object()
+Person3.FirstName = "Rakesh",
+    Person3.LastName = "Kumar",
+    Person3.OfficeAddress = "Saharsa,Bihar"
+
+console.log("Person1:");
+console.log("FirstName: " + Person1.FirstName);
+console.log("LastName: " + Person1.LastName);
+console.log("OfficeAddress: " + Person1.OfficeAddress);
+
+console.log("Person2:");
+console.log("FirstName: " + Person2.FirstName);
+console.log("LastName: " + Person2.LastName);
+console.log("OfficeAddress: " + Person2.OfficeAddress);
+
+console.log("Person3:");
+console.log("FirstName: " + Person3.FirstName);
+console.log("LastName: " + Person3.LastName);
+console.log("OfficeAddress: " + Person3.OfficeAddress);
